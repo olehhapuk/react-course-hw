70c9d34ad6f9342dd3eb5acfa84c20b761517145
Sed225 <pavlolehiv@gmail.com>
2025-11-17 01:32:27 +0200

    hw01 fix

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,55 +1,47 @@
+import PostsList from "./posts-list";
 import PostCard from "./post-card";
-import styles from "./app.module.css";
 
 export default function App() {
   return (
-    <div className={styles.card_list}>
-      <div>
-        <PostCard
-          avatarUrl="/public/Profile Image.png"
-          displayName="John Doe"
-          username="@johndoe"
-          createdAt="01.11.2025"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo neque, condimentum ac tellus id, ullamcorper commodo ante."
-          imageUrl="/public/Thumbnail Image.png"
-          isOnline
-          isLiked={false}
-        />
-      </div>
-      <div>
-        <PostCard
-          avatarUrl="/public/Profile Image.png"
-          displayName="John Doe"
-          username="@johndoe"
-          createdAt="01.11.2025"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo neque, condimentum ac tellus id, ullamcorper commodo ante."
-          imageUrl="/public/Thumbnail Image.png"
-          isOnline={false}
-          isLiked
-        />
-      </div>
-      <div>
-        <PostCard
-          avatarUrl="/public/Profile Image.png"
-          displayName="John Doe"
-          username="@johndoe"
-          createdAt="01.11.2025"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo neque, condimentum ac tellus id, ullamcorper commodo ante."
-          isOnline={false}
-          isLiked={false}
-        />
-      </div>
-      <div>
-        <PostCard
-          avatarUrl="/public/Profile Image.png"
-          displayName="John Doe"
-          username="@johndoe"
-          createdAt="01.11.2025"
-          imageUrl="/public/Thumbnail Image.png"
-          isOnline={false}
-          isLiked={false}
-        />
-      </div>
-    </div>
+    <PostsList>
+      <PostCard
+        avatarUrl="/public/Profile Image.png"
+        displayName="John Doe"
+        username="@johndoe"
+        createdAt="01.11.2025"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo neque, condimentum ac tellus id, ullamcorper commodo ante."
+        imageUrl="/public/Thumbnail Image.png"
+        isOnline
+        isLiked={false}
+      />
+      <PostCard
+        avatarUrl="/public/Profile Image.png"
+        displayName="John Doe"
+        username="@johndoe"
+        createdAt="01.11.2025"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo neque, condimentum ac tellus id, ullamcorper commodo ante."
+        imageUrl="/public/Thumbnail Image.png"
+        isOnline={false}
+        isLiked
+      />
+      <PostCard
+        avatarUrl="/public/Profile Image.png"
+        displayName="John Doe"
+        username="@johndoe"
+        createdAt="01.11.2025"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur justo neque, condimentum ac tellus id, ullamcorper commodo ante."
+        isOnline={false}
+        isLiked={false}
+      />
+      <PostCard
+        avatarUrl="/public/Profile Image.png"
+        displayName="John Doe"
+        username="@johndoe"
+        createdAt="01.11.2025"
+        imageUrl="/public/Thumbnail Image.png"
+        isOnline={false}
+        isLiked={false}
+      />
+    </PostsList>
   );
 }
--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -1,5 +1,9 @@
+import styles from "./app.module.css";
+
 interface PostsListProps {
   children: React.ReactNode;
 }
 
-export default function PostsList({ children }: PostsListProps) {}
+export default function PostsList({ children }: PostsListProps) {
+  return <div className={styles.card_list}>{children}</div>;
+}
